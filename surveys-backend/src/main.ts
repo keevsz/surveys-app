da5a593cd523956d7f3d271c1ac973fce620e83a
unknown <keviv1q2@gmail.com>
2023-01-28 11:09:18 -0500

    configuración de cookie session

--- surveys-backend/src/main.ts
+++ surveys-backend/src/main.ts
@@ -7,17 +7,22 @@ import { DataSource } from 'typeorm';
 import { Session } from './auth/entities/Auth.entity';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:5173', credentials: true },
+  });
   const sessionRepository = app.get(DataSource).getRepository(Session);
   app.setGlobalPrefix('api');
   app.use(
     session({
       name: 'SESSION_NESTJS',
-      secret: 'keevsz',
+      secret: 'SECRET_SESSION',
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 60000,
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false,
+        expires: new Date(Date.now() + 60 * 60 * 1000),
+        sameSite: 'lax',
       },
       store: new TypeormStore().connect(sessionRepository),
     }),
