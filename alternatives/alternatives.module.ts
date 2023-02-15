89786247be15190aad80166e460220ee61d0ef2d
unknown <keviv1q2@gmail.com>
2023-02-15 12:39:30 -0500

    fixing

--- alternatives/alternatives.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AlternativesService } from './alternatives.service';
-import { AlternativesController } from './alternatives.controller';
-
-@Module({
-  controllers: [AlternativesController],
-  providers: [AlternativesService]
-})
-export class AlternativesModule {}
